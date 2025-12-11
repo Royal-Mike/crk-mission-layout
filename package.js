2d4ef79d0e0e119d1cf50972f2f106eaaa0545f8
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2025-12-12 01:19:07 +0700

    new holiday packages

--- package.js
+++ package.js
@@ -1,81 +1,119 @@
 const packages = [
     {
-        name:"Jolly Gift", note:"Thank you for being here with us!",
-        background:"holidays/background0", cookie:"cookie0", currency:false, price:"FREE",
-        r_gem:1, r_pass:1,
-        footer:"Redeem this free package by pinging <span class='discord_mention'>@royalmike</span> in <span class='discord_mention'>#crumbtalk</span>."
+        name:"Jolly Gift",
+        header:"Happy Holidays!",
+        note:"Thank you for 4 Years of CrumbSeeking!<br>Take this FREE package as a token of our gratitude!",
+        footer:"Redeem this package by pinging <span class='discord_mention'>@royalmike</span> in <span class='discord_mention'>#crumbtalk</span>.",
+        background:"holidays/background0", cookie:"holidays/cookie0", currency:false, price:"FREE",
+        colors:{
+            header: "#93caf6",
+            details:{
+                header: "#6379c4c2", item:"#5881d0", image:"#2e426a"
+            }
+        },
+        details:[
+            {name:"Tokens", image:"token", count:50},
+            {name:"Gem", image:"gem", count:1},
+            {name:"Friend Cards", image:"card", count:2}
+        ]
     },
     {
-        name:"Casual Holiday Package", note:"Add a Cookie to CrumbSeeker with this package!",
-        background:"holidays/background1", cookie:"cookie1", currency:"token", price:"50",
-        gem:1, r_cube:1, r_cog:5, r_badge:2,
-        footer:"Use the <span class='discord_mention'>em/pay</span> command in <span class='discord_mention'>#crumbtalk</span> to pay for this package."
+        name:"Welcome Package",
+        header:"Newbie Bonus!",
+        note:"Are you new to CrumbSeeker and our community?<br>Take this FREE package and submit your Cookies to the game!",
+        footer:"This package is <span class='discord_mention'>automatically</span> redeemed for those who currently have <span class='discord_mention'>5 or less</span> Cookies in CrumbSeeker.",
+        background:"holidays/background1", cookie:"holidays/cookie1", currency:false, price:"FREE",
+        colors:{
+            header: "#9ddbc9ff",
+            details:{
+                header: "#73b5509c", item:"#88c9b5", image:"#4f7569"
+            }
+        },
+        details:[
+            {name:"HyperCubes", image:"cube", count:3},
+            {name:"V.S.C.", image:"vsc", count:1}
+        ]
     },
     {
-        name:"Premium Holiday Package", note:"Add Dungeon Movesets to CrumbSeeker Cookies with this package!", width:"325px",
-        background:"holidays/background2", cookie:"cookie2", currency:"money", price:"5",
-        r_vsc:3, r_medal:5, r_searing:2,
-        footer:"Donate to Mike's <span class='discord_mention'>Ko-fi</span>, then ping <span class='discord_mention'>@royalmike</span> in <span class='discord_mention'>#crumbtalk</span> to receive this package."
+        name:"Holiday Package",
+        header:"Great Offer!",
+        note:"Purchase this package with ONLY a Pass<br>and receive amazing value for the cost!",
+        footer:"Purchase this package by using the <span class='discord_mention'>em/pay</span> command in <span class='discord_mention'>#commands</span>.",
+        notice:"2 per member (2/2)",
+        background:"holidays/background2", cookie:"holidays/cookie2", currency:"pass", price:"1", method4:true,
+        colors:{
+            header: "#ffff60ff",
+            details:{
+                header: "#7b5025b5", item:"#7e5e32", image:"#3e2e18"
+            }
+        },
+        details:[
+            {name:"HyperCube", image:"cube", count:1},
+            {name:"Medals", image:"medal", count:10},
+            {name:"Epic Friend Card", image:"card_epic", count:1}
+        ]
     },
     {
-        name:"Artistic Package", note:"Draw an art to receive awesome gifts!", width:"325px",
-        background:"holidays/background3", cookie:"cookie3", currency:"art", price:"1",
-        r_cube:2, r_vsc:2, r_special:1,
-        footer:"Submit to the <span class='discord_mention'>Fan-Art Box</span>, then ping <span class='discord_mention'>@royalmike</span> in <span class='discord_mention'>#crumbtalk</span> to receive this package."
-    },
+        name:"Ko-fi Package",
+        header:"Support Us!",
+        note:"This package costs real money to purchase,<br>but the rewards are great if you do decide to get it!",
+        footer:"Purchase this package by donating the price to Mike's <span class='discord_mention'>Ko-fi</span>.",
+        background:"holidays/background3", cookie:"holidays/cookie3", currency:false, price:"$9.98", method4:true,
+        colors:{
+            header: "#ff943cff",
+            details:{
+                header: "#e84e2796", item:"#c9885a", image:"#795237"
+            }
+        },
+        details:[
+            {name:"V.S.C.s", image:"vsc", count:3},
+            {name:"Searing Badge", image:"searing", count:1},
+            {name:"Legendary Friend Card", image:"card_legendary", count:1}
+        ]
+    }
 ];
 
-const current = packages[1];
+let index = 0;
 
-$(".background").css("background-image", `url(images/${current.background}.png)`);
-$("h1").text(current.name);
-$(".text_title_note").text(current.note);
-$(".text_note").html(current.footer).css("width", current.width);
-$(".cookie_package").attr("src", `images/${current.cookie}.png`);
-$(".button.package > p").text(current.price.toLocaleString());
+$("body").on("click", function() {
+    index++;
+    if (index === packages.length) index = 0;
+    updatePackage(packages[index]);
+});
 
-if (!current.currency) {
-    $(".button.package > img").hide();
-}
-else {
-    $(".button.package > img").attr("src", `images/${current.currency}.png`);
-}
+updatePackage(packages[index]);
 
-let count = 0;
-for (let key in current) {
-    if (!key.startsWith("r_")) continue;
+function updatePackage(package) {
+    $(".background").css("background-image", `url(images/${package.background}.png)`);
+    $("h1").text(package.name);
+    $("h3").text(package.header).css("color", package.colors.header);
+    $(".text_title_note").html(package.note);
+    $(".text_note").html(package.footer).css("width", package.width);
+    $(".package_cookie").attr("src", `images/${package.cookie}.png`);
+    $(".button.package > .flex > .text_price").text(package.price.toLocaleString());
+    $(".header").css("background-color", package.colors.details.header);
 
-    let name = key.replace("r_", "");
+    if (!package.notice) $(".button.package > .text_notice").text("1 per member");
+    else $(".button.package > .text_notice").text(package.notice);
 
-    switch (key) {
-        case "r_cog":
-            name = "M. Cog"; break;
-        case "r_special":
-            name = "Sp. Badge"; break;
-        case "r_badge":
-            name = "B. Badge"; break;
-        case "r_searing":
-            name = "Sr. Badge"; break;
-        case "r_vsc":
-            name = "V.S.C"; break;
-    }
+    if (!package.currency) $(".button.package > .flex > img").hide();
+    else $(".button.package > .flex > img").show().attr("src", `images/${package.currency}.png`);
 
-    if (current[key] > 1) {
-        if (name.endsWith("s")) name += "es";
-        else name += "s";
-    }
+    if (package.method4) $(".method4").show();
+    else $(".method4").hide();
 
-    $(".package_content").append(
-        `<div class="flex flexcol item">
-            <img src="images/${key.replace("r_", "")}.png">
-            <p class="text_border text_small">${name}</p>
-            <p class="text_border">x${current[key]}</p>
-        </div>`
-    );
-
-    count++;
-}
-
-if (count > 4) {
-    $(".package_content > .item").addClass("package_item_small");
+    $(".package_details > .content").empty();
+    package.details.forEach(item => {
+        $(".package_details > .content").append(
+            `<div class="flex item" style="background-color:${package.colors.details.item}">
+                <div class="flex image" style="background-color:${package.colors.details.image}">
+                    <img src="images/${item.image}.png">
+                </div>
+                <div class="flex flexcol">
+                    <p class="text_border text_small">${item.name}</p>
+                    <p class="text_border">${item.count}</p>
+                </div>
+            </div>`
+        );
+    });
 }
